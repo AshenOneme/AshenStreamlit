24147afbaa6011bd117873e02604916ba24d38da
AshenOneme <98397090+AshenOneme@users.noreply.github.com>
2023-12-21 12:46:06 +0800

    Add files via upload

---
+++ pages/Hysteresis curve.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Hysteresis Curve", page_icon="📊")
+
+st.markdown("# Hysteresis Curve")
+st.sidebar.header("Hysteresis Curve")
+file = st.file_uploader("Pick a file")
