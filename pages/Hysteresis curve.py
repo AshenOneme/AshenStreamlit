1dbe41c01daa488dccf0c3d200af76b6e07d399f
AshenOneme <98397090+AshenOneme@users.noreply.github.com>
2023-12-21 12:55:54 +0800

    Update Hysteresis curve.py

--- pages/Hysteresis curve.py
+++ pages/Hysteresis curve.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
 import altair as alt
 from urllib.error import URLError
@@ -8,4 +9,5 @@ st.set_page_config(page_title="Hysteresis Curve", page_icon="📊")
 st.markdown("# Hysteresis Curve")
 st.sidebar.header("Hysteresis Curve")
 file = st.file_uploader("Pick a file")
-print(file)
+plt.plot(file)
+plt.show()
