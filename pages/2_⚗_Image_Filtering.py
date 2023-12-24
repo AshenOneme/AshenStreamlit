2a365d3fe6e6a4fdc59fe13f682626d26168e46a
AshenOneme <98397090+AshenOneme@users.noreply.github.com>
2023-12-24 19:14:06 +0800

    Delete pages/2_⚗_Image_Filtering.py

--- pages/2_⚗_Image_Filtering.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-import plotly.graph_objs as go
-import streamlit as st
-import numpy as np
-
-st.set_page_config(page_title="Merry Christmas! Everyone in office 513.", page_icon="⚗")
-
-st.markdown("# Merry Christmas! Everyone in office 513.")
-st.sidebar.header("Merry Christmas! Everyone in office 513.")
